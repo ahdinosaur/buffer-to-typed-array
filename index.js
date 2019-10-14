c4e201bf48b1866475348cf5dd879b56b2192e84
mk-pmb <mk-pmb@users.noreply.github.com>
2019-10-14 03:40:35 +0200

    Convert TypedArray directly, using .byteOffset
    Closes #5

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 var getDataType = require('dtype')
-var toUint8 = require('buffer-to-uint8array')
 
 module.exports = bufferToTypedArray
 
 function bufferToTypedArray (dtype) {
   var TypedArray = getDataType(dtype)
   return function (buf) {
-    return new TypedArray(toUint8(buf).buffer)
+    var n = Math.floor(buf.length / TypedArray.BYTES_PER_ELEMENT)
+    return new TypedArray(buf.buffer, buf.byteOffset, n)
   }
 }
